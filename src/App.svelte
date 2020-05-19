e0a9670009674b22ceecba94c4936d0024cd36b1
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-05-18 20:41:47 -0500

    Added Initial grid

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+  const GRID_WIDTH = 10;
+  const GRID_HEIGHT = 20;
+  const GRID_SIZE = GRID_HEIGHT * GRID_WIDTH;
+
+  const squares = Array.from({ length: GRID_SIZE }, (_, i) => ({
+    index: i,
+    color: "blue",
+  }));
+
+  console.log(squares);
+
+  function draw(tetrimino) {
+    const currentPosition = 4;
+    tetrimino.forEach(
+      (index) => (squares[index + currentPosition].color = "red")
+    );
+  }
+
+  const tTetromino = [
+    [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2],
+    [1, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+    [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+    [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+  ];
+
+  draw(tTetromino[0]);
+</script>
+
+<style>
+  main {
+    /* background: red; */
+  }
+
+  .block {
+    background: red;
+  }
+
+  section {
+    display: grid;
+    grid-template-rows: repeat(20, 10px);
+    grid-template-columns: repeat(10, 10px);
+    gap: 1px;
+  }
+
+  div {
+    /* background: blue; */
+  }
+</style>
+
+<main>
+  <section>
+    {#each squares as { index, color }, i}
+      <div style="background: {color}" />
+    {/each}
+  </section>
+</main>
