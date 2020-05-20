0cb5b4d39ec15a1a49925cd5208c32e5e6701fc9
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-05-20 18:28:23 -0500

    Added instructions text

--- src/components/Instructions.svelte
+++ src/components/Instructions.svelte
@@ -1,17 +1,23 @@
+<script>
+  import Game from "./Game.svelte";
+</script>
+
 <style>
   .instructionsContainer {
     height: 80%;
     max-width: 30vmax;
     margin: 0 5vmax;
   }
+  .instructionKey {
+    font-weight: bold;
+    color: hsl(198, 52%, 12%);
+  }
   h1 {
-    /* font-size: 72px; */
     font-size: calc(3vmax + 12px);
     font-family: "Press Start 2P", cursive;
     color: hsl(198, 86%, 12%);
   }
   p {
-    /* font-size: 24px; */
     font-size: calc(1vmax + 12px);
   }
 </style>
@@ -19,11 +25,14 @@
 <div class="instructionsContainer">
   <h1>Tetris</h1>
   <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ex ante,
-    fringilla ut ipsum a, consequat scelerisque lectus
+    This is a very basic tetris game. You can use
+    <span class="instructionKey">arrow keys</span>
+    to move and rotate blocks. Game ends when a new piece don't fit in the
+    screen. Enjoy!
   </p>
   <p>
-    Quisque hendrerit tortor vitae porta efficitur. Nam vestibulum tortor at
-    mauris tincidunt, eget dapibus massa auctor. In eu laoreet arcu.
+    Press
+    <span class="instructionKey">Enter Key</span>
+    to start playing.
   </p>
 </div>
